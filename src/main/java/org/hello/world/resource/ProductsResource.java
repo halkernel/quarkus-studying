fda6bf5facf802198ef735ba1dc294358ee710d9
halkernel <kennet.emerson@gmail.com>
2020-05-03 13:22:52 -0300

    testing integration with pgsql and native executable

--- src/main/java/org/hello/world/resource/ProductsResource.java
+++ src/main/java/org/hello/world/resource/ProductsResource.java
@@ -5,14 +5,15 @@ import org.hello.world.entity.Product;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.LinkedList;
 import java.util.List;
 
 @Path("/products")
-public class ProductResource {
+@Transactional
+public class ProductsResource {
 
     @Inject
     EntityManager entityManager;
@@ -24,5 +25,11 @@ public class ProductResource {
     @Produces(value = MediaType.APPLICATION_JSON)
     public List<Product> getProducts(){
         return entityManager.createQuery("select p from Product p", Product.class).getResultList();
+    }
+
+    @POST
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    public void addProduct(Product p){
+        entityManager.persist(p);
     }
 }
